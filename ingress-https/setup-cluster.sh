fea34e76d8f212c0af9b69ff89461e8ef23ad1fc
Nills Franssens <nilfran@microsoft.com>
2021-04-07 13:20:46 -0700

    ingress-https

--- ingress-https/setup-cluster.sh
+++ ingress-https/setup-cluster.sh
@@ -2,4 +2,4 @@ RGNAME=ingress-https
 AKSNAME=ingress-https
 
 az group create -l westus2 -n $RGNAME
-az aks create -n $AKSNAME -g $RGNAME --kubernetes-version 1.19
+az aks create -n $AKSNAME -g $RGNAME --kubernetes-version 1.19.7
